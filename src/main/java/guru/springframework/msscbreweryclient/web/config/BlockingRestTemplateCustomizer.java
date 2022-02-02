e25b0e68471b8ae613dbb883a05ccb1eddca8290
Sam Smith <samtsmith@gmail.com>
2022-02-02 17:45:10 +0000

    Application 3: adding another solution to the problem just for giggles

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -20,17 +20,31 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
-    @Value("${a.h.c.maxTotal}")
-    private int maxTotal;
+    //@Value("${a.h.c.maxTotal}")
+    private final int maxTotal;
 
-    @Value("${a.h.c.defaultMaxPerRoute}")
-    private int defaultMaxPerRoute;
+    //@Value("${a.h.c.defaultMaxPerRoute}")
+    private final int defaultMaxPerRoute;
 
-    @Value("${a.h.c.connectionRequestTimeout}")
-    private int connectionRequestTimeout;
+    //@Value("${a.h.c.connectionRequestTimeout}")
+    private final int connectionRequestTimeout;
 
-    @Value("${a.h.c.socketTimeout}")
-    private int socketTimeout;
+    //@Value("${a.h.c.socketTimeout}")
+    private final int socketTimeout;
+
+    /*
+     * NOTE: The annotations on the properties above is one solution (see commented above).
+     *       Constructor with annotations on the args is another solution (see below).
+     */
+    public BlockingRestTemplateCustomizer(@Value("${a.h.c.maxTotal}") int maxTotal,
+                                          @Value("${a.h.c.defaultMaxPerRoute}") int defaultMaxPerRoute,
+                                          @Value("${a.h.c.connectionRequestTimeout}") int connectionRequestTimeout,
+                                          @Value("${a.h.c.socketTimeout}") int socketTimeout) {
+        this.maxTotal = maxTotal;
+        this.defaultMaxPerRoute = defaultMaxPerRoute;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
 
     public ClientHttpRequestFactory clientHttpRequestFactory(){
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
